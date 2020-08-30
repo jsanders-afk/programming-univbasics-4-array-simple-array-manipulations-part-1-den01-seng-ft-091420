fa3450db972e48e32111de4a6de24e8f2ace0a4d
Jack Sanders <guitarbreakerjs@gmail.com>
2020-08-30 23:11:05 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array,string)
+  array = []
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  ara = array.pop
+  ara
+end
+
+def pop_with_args(array)
+  ar2 = array.pop[2]
+  ar2.pop
+end
+  
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift
+  array
+end
